60b018026562e5588fe1336b87f11d26bb681ef8
Idan-Magled <idan.magled@naturalint.com>
2019-08-09 12:34:27 +0300

    Update PaymentCardView.java

--- paymentcardview/src/main/java/com/manojbhadane/PaymentCardView.java
+++ paymentcardview/src/main/java/com/manojbhadane/PaymentCardView.java
@@ -21,8 +21,8 @@ public class PaymentCardView extends RelativeLayout {
     private ImageView mImgCard;
     private TextView mTxtCardTitle;
     private Typeface RegularTypeFace;
-    private EditText mEdtCardNumber, mEdtCvv;
-    private EditText mSpinnerMonth, mSpinnerYear;
+    EditText mEdtCardNumber, mEdtCvv;
+    EditText mSpinnerMonth, mSpinnerYear;
     private OnPaymentCardEventListener mListener;
 
     /**
